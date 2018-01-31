5f170094f8a353181dd72458f0bf8f81651b2ea0
Lindsay Dennis <lindsay.c.dennis@gmail.com>
2018-01-31 14:22:23 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,8 +1,12 @@
 def fizzbuzz(int)
  if (int % 3 == 0) && (int % 5 == 0)
-    puts "FizzBuzz"
+    return "FizzBuzz"
  elsif (int % 3 == 0)
-   puts "Fizz"
+    return "Fizz"
+ elsif (int % 5 == 0)
+    return "Buzz"
+ else
+    return nil 
   end
 end
 
